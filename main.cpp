265da274d194d2d9aa4335ee6d157d9c3170ed3c
That One Seong <7321839+SeongGino@users.noreply.github.com>
2024-05-11 00:39:13 -0400

    gweh

--- main.cpp
+++ main.cpp
@@ -28,7 +28,7 @@ int main(int argc, char *argv[])
     QTranslator translator;
     const QStringList uiLanguages = QLocale::system().uiLanguages();
     for (const QString &locale : uiLanguages) {
-        const QString baseName = "G4A-GUImain_" + QLocale(locale).name();
+        const QString baseName = "AppTranslations_" + QLocale(locale).name();
         if (translator.load(":/i18n/" + baseName)) {
             a.installTranslator(&translator);
             break;
